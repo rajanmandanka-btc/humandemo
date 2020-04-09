e9c3cdf51cd0f42d3c526d645ef5ef8963461ece
NipunBrahmbhatt <nipun.brahmbhatt@botreetechnologies.com>
2020-04-09 15:40:40 +0530

    Changes in views

--- human/demo/serializers.py
+++ human/demo/serializers.py
@@ -2,12 +2,7 @@ from rest_framework import serializers
 from .models import FileUpload
 
 
-class FileUploadSerializer(serializers.HyperlinkedModelSerializer):
-    owner = serializers.SlugRelatedField(
-        read_only=True,
-        slug_field='id'
-    )
-
+class FileUploadSerializer(serializers.ModelSerializer):
     class Meta:
         model = FileUpload
         fields = '__all__'
--- human/demo/views.py
+++ human/demo/views.py
@@ -4,7 +4,7 @@ from rest_framework.parsers import FormParser, MultiPartParser
 from rest_framework.viewsets import ModelViewSet
 from .models import FileUpload
 from .serializers import FileUploadSerializer
-from subprocess import Popen, PIPE
+import ffmpeg
 
 
 # Create your views here.
@@ -18,19 +18,32 @@ class FileUploadViewSet(ModelViewSet):
     def perform_create(self, serializer):
         print("data-----------------", )
         content_file = self.request.data.get('datafile')
+        print(content_file)
 
-        with open(content_file, "rb") as infile:
-            p = Popen(["ffmpeg", "-i", "-", "-f", "matroska", "-vcodec", "mpeg4",
-                       "-acodec", "aac", "-strict", "experimental", "-"],
-                      stdin=infile, stdout=PIPE)
-            while True:
-                data = p.stdout.read(1024)
-                if len(data) == 0:
-                    break
-                # do something with data...
-                print(data)
-                print("=============> ", "finished ------ ")
-            p.wait()  # should have finisted anyway
+        try:
+            (ffmpeg.input('test.mp4')
+             .filter('fps', fps=2)
+             .output('test/%d.png',
+                     video_bitrate='5000k',
+                     s='64x64',
+                     sws_flags='bilinear',
+                     start_number=0)
+             .run(capture_stdout=True, capture_stderr=True))
+        except ffmpeg.Error as e:
+            print('stdout:', e.stdout.decode('utf8'))
+            print('stderr:', e.stderr.decode('utf8'))
 
-        serializer.save(owner=self.request.user,
-                        datafile=self.request.data.get('datafile'))
+        # with open(content_file, "rb") as infile:
+        #     p = Popen(["ffmpeg", "-i", "-", "-f", "matroska", "-vcodec", "mpeg4",
+        #                "-acodec", "aac", "-strict", "experimental", "-"],
+        #               stdin=infile, stdout=PIPE)
+        #     while True:
+        #         data = p.stdout.read(1024)
+        #         if len(data) == 0:
+        #             break
+        #         # do something with data...
+        #         print(data)
+        #         print("=============> ", "finished ------ ")
+        #     p.wait()  # should have finisted anyway
+
+        serializer.save(datafile=self.request.data.get('datafile'))
